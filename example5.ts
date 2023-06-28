bd183cf29aea720ae5f28697ba4fa7ce0506fddc
sandipmohapatra <sandipmohapatra@users.noreply.github.com>
2023-06-28 16:53:54 +0530

    Add files via upload

---
+++ example5.ts
@@ -0,0 +1,10 @@
+let x="This is Angular13.It is easy";
+if(x.startsWith("This"))
+console.log("the sentence starts with this");
+else
+console.log("the doesnot sentence starts with this");
+
+if(x.endsWith("easy"))
+console.log("the sentence ends with easy");
+else
+console.log("the doesnot sentence ends with easy");
